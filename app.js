aefdc580a811352839fb1969a71860eea437bf60
Pranjali-04-hash <pranjalimutagekar8@gmail.com>
2026-01-06 23:15:41 +0530

    Final working student registration app with MongoDB

--- app.js
+++ app.js
@@ -1,34 +1,70 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 const session = require("express-session");
+
+const User = require("./models/User");
 
 const app = express();
 
 // Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
+
+app.set("view engine", "ejs");
 
 app.use(
   session({
     secret: "student_secret",
     resave: false,
-    saveUninitialized: true,
+    saveUninitialized: true
   })
 );
 
-// View engine
-app.set("view engine", "ejs");
-
-// MongoDB connection
+// MongoDB Connection
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.log(err));
+  .catch(err => console.log(err));
 
 // Routes
 app.get("/", (req, res) => {
-  res.send("Student Course Registration System Running");
+  res.redirect("/login");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+app.post("/register", async (req, res) => {
+  const { name, email, password } = req.body;
+  const hashed = await bcrypt.hash(password, 10);
+
+  await User.create({ name, email, password: hashed });
+  res.redirect("/login");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+
+  if (user && await bcrypt.compare(password, user.password)) {
+    req.session.user = user;
+    res.redirect("/home");
+  } else {
+    res.send("Invalid credentials");
+  }
+});
+
+app.get("/home", (req, res) => {
+  if (!req.session.user) return res.redirect("/login");
+  res.render("home", { user: req.session.user });
 });
 
 // Server
